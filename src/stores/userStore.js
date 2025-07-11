8f79963a6d644b3d43c2661d61ee1d2cb7fa78ad
Arsyl-TI <arsilsyahrowi@gmail.com>
2025-07-11 13:02:26 +0700

    json 2

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://fake-json-chi.vercel.app/users'
 
 export const useUserStore = defineStore('user', () => {
   const user = ref(null)
