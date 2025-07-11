056fdf561f1d67a9fa6fcc63f313486ceb7c3580
Arsyl-TI <arsilsyahrowi@gmail.com>
2025-07-11 12:59:32 +0700

    perbaikan json

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3004'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const useUserStore = defineStore('user', () => {
   const user = ref(null)
